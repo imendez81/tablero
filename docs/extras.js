e22dfe2baac8ce98cdd3b72b1f6c387f2280d589
Ignacio <44736217+imendez81@users.noreply.github.com>
2024-04-16 09:39:41 -0300

    Add files via upload

---
+++ docs/extras.js
@@ -0,0 +1,166 @@
+
+const FechaActualizacion = "16/04/2024 09:20:00"
+
+//-----------------TONERS-----------------------------//
+                                        // Tóner HP Laser Jet 3005 - Mat. 290
+                                                  var data290 = [4, 1, 14, 10];
+var stockActual290 = 1168-(data290[(data290.length-1)]);
+                                                  // Tóner LEXMARK - Mat. 296
+                                                   var data296 = [3, 34, 0, 0];
+var stockActual296 = 3-(data296[(data296.length-1)]);
+                                               // Tóner Brother HL - Mat. 337
+                                                    var data337 = [2, 0, 6, 0];
+var stockActual337 = 219-(data337[(data337.length-1)]);
+                                              // Tóner Ricoh 4500 - Mat. 2078
+                                                   var data2078 = [0, 0, 0, 0];
+var stockActual2078 = 0-(data2078[(data2078.length-1)]);
+                         // TÓNER P/IMPRESORA SAMSUNG 5010 ND/XBG - Mat. 3977
+                                                var data3977 = [29, 8, 30, 18];
+var stockActual3977 = 59-(data3977[(data3977.length-1)]);
+                             // CARTUCHO DE TONER LEXMARK MS823DN - Mat. 6825
+                                                  var data6825 = [0, 35, 0, 0];
+var stockActual6825 = 3-(data6825[(data6825.length-1)]);
+
+//-----------------RESMAS-----------------------------//
+                                         // Papel Obra Oficio 80gr - Mat. 786
+                                                    var data786 = [0, 400, 0, 0];
+var stockActual786 = 0-(data786[(data786.length-1)]);
+                                             // Papel Obra A4 80gr - Mat. 793
+                                                      var data793 = [0, 0, 0, 0];
+var stockActual793 = 0-(data793[(data793.length-1)]);
+                                                 // Papel Obra A3 - Mat. 3360
+                                             var data3360 = [500, 300, 450, 100];
+var stockActual3360 = 8155-(data3360[(data3360.length-1)]);
+                                                 // Papel Obra A4 - Mat. 3361
+                                      var data3361 = [11120, 23800, 25960, 12540];
+var stockActual3361 = 39710-(data3361[(data3361.length-1)]);
+
+//-----------------LIBRERIA-----------------------------//
+                                  // ABROCHADORA METÁLICA 21/6 Ó 21/8 - Mat. 83
+                                                      var data83 = [2, 0, 0, 0];
+var stockActual83 = 0-(data83[(data83.length-1)]);
+                                   // ABROCHADORAS  TIPO PINZA  Nº 50 - Mat. 85
+                                                      var data85 = [0, 0, 0, 0];
+var stockActual85 = 0-(data85[(data85.length-1)]);
+                                                   // Bolígrafo Azul - Mat. 145
+                                                     var data145 = [0, 0, 0, 0];
+var stockActual145 = 0-(data145[(data145.length-1)]);
+                                     // BROCHES 2 PATAS DORADOS, N°8 - Mat. 162
+                                             var data162 = [495, 172, 316, 200];
+var stockActual162 = 3005-(data162[(data162.length-1)]);
+                                     // BROCHES PARA ABROCHADORA N50 - Mat. 167
+                                                     var data167 = [0, 0, 0, 0];
+var stockActual167 = 0-(data167[(data167.length-1)]);
+                                 // BROCHES P/ABROCHAD. 21/6 cjx1000 - MAT. 169
+                                                     var data169 = [0, 0, 0, 0];
+var stockActual169 = 0-(data169[(data169.length-1)]);
+                         // CAJA DE ARCHIVO, CARTON OFICIO ALTO 12CM - MAT. 203
+                                                     var data203 = [0, 0, 0, 0];
+var stockActual203 = 0-(data203[(data203.length-1)]);
+                                       // CARPETA CARTÓN, 24 x 34 cm - MAT. 234
+                                             var data234 = [216, 319, 291, 136];
+var stockActual234 = 4125-(data234[(data234.length-1)]);
+                                // CARPETA PVC  TAPA TRANSPARENTE A4 - MAT. 243
+                                                     var data243 = [0, 0, 0, 0];
+var stockActual243 = 0-(data243[(data243.length-1)]);
+                              // COLA TRANSPARENTE C/DOSIFICADOR 30ML - MAT. 494
+                                                    var data494 = [20, 0, 0, 0];
+var stockActual494 = 0-(data494[(data494.length-1)]);
+                         // CUADERNO 84H 21X29.7CM.RAY, T DUR. ESPIR - MAT. 521
+                                                     var data521 = [0, 0, 0, 0];
+var stockActual521 = 0-(data521[(data494.length-1)]);
+                                 // CUADERNO 84H CUAD. A4 ESPIR 70GR - MAT. 522
+                                                     var data522 = [0, 0, 0, 0];
+var stockActual522 = 0-(data522[(data494.length-1)]);
+                                       // GOMA DE BORRAR LAPIZ TINTA - Mat. 630
+                                                     var data630 = [6, 0, 0, 0];
+var stockActual630 = 0-(data630[(data630.length-1)]);
+                                      // Lápiz negro grafito alta cal - Mat. 680
+                                                     var data680 = [0, 0, 0, 0];
+var stockActual680 = 0-(data680[(data680.length-1)]);
+                                          // Libro de Actas Tapa dura - Mat. 685
+                                                    var data685 = [14, 0, 0, 0];
+var stockActual685 = 0-(data685[(data685.length-1)]);
+                                       // LIQUIDO CORRECTOR EN FRASCO - Mat. 689
+                                                     var data689 = [0, 0, 0, 0];
+var stockActual689 = 0-(data689[(data689.length-1)]);
+                                            // Rollo de Papel Térmico - Mat. 796
+                                             var data796 = [515, 396, 327, 142];
+var stockActual796 = 20135-(data796[(data796.length-1)]);
+                              // REGLA PLASTICA TRANSPARENTE DE 30 CM. - Mat. 833
+                                                     var data833 = [0, 0, 0, 0];
+var stockActual833 = 0-(data833[(data833.length-1)]);
+                                      // Resaltador Amarillo Trazo GR - Mat. 836
+                                          var data836 = [2347, 1353, 1429, 749];
+var stockActual836 = 7111-(data836[(data836.length-1)]);
+                                                // Resaltador Celeste - Mat. 837
+                                                     var data837 = [0, 0, 0, 0];
+var stockActual837 = 0-(data837[(data837.length-1)]);
+                                        // Resaltador Fucsia Trazo GR - Mat. 838
+                                                     var data838 = [0, 0, 0, 0];
+var stockActual838 = 0-(data838[(data838.length-1)]);
+                                                  // Resaltador Verde - Mat. 839
+                                                     var data839 = [0, 0, 0, 0];
+var stockActual839 = 0-(data839[(data839.length-1)]);
+                                                           // Tijera - Mat. 972
+                                                     var data972 = [0, 0, 0, 0];
+var stockActual972 = 0-(data972[(data972.length-1)]);
+                                                // TINTA PARA SELLOS - Mat. 977
+                                                     var data977 = [0, 0, 0, 0];
+var stockActual977 = 0-(data977[(data977.length-1)]);
+                                                // CINTA DE EMBALAR - Mat. 1355
+                                                     var data1355 = [0, 0, 0, 0];
+var stockActual1355 = 0-(data1355[(data1355.length-1)]);
+                                      // Bolígrafo descartable negro - Mat. 3495
+                                                    var data3495 = [0, 0, 0, 0];
+var stockActual3495 = 0-(data3495[(data3495.length-1)]);
+                                     // Taco de Papel 9x9 Multicolor - Mat. 3564
+                                                    var data3564 = [0, 0, 0, 0];
+var stockActual3564 = 0-(data3564[(data3564.length-1)]);
+
+//-----------------COMPUTADORAS-----------------------------//
+                                             // COMPUTADORA PORTATIL - MAT. 6235
+                                                     var data6235 = [1, 0, 0, 0];
+var stockActual6235 = 4-(data6235[(data6235.length-1)]);
+                              // COMPUTADORA ESC AVANZADA - REND-MED - MAT. 6236
+                                                     var data6236 = [0, 1, 7, 0];
+var stockActual6236 = 246-(data6236[(data6236.length-1)]);
+                            // COMPUTADORA PORTATIL DELGADA Y LIVIANA - MAT. 6270
+                                                     var data6270 = [0, 0, 0, 0];
+var stockActual6270 = 4-(data6270[(data6270.length-1)]);
+                               // COMPUTADORA ESCRITORIO AVANZADA-AIO - MAT. 6271
+                                                     var data6271 = [1, 0, 0, 1];
+var stockActual6271 = 26-(data6271[(data6271.length-1)]);
+
+//-----------------COVID-----------------------------//
+                                             // ALCOHOL EN GEL 500CM3 - Mat. 763
+                                                      var data763 = [0, 0, 0, 0];
+var stockActual763 = 0-(data763[(data763.length-1)]);
+                                             // Barbijo Descartable - Mat. 2239
+                                                  var data2239 = [13, 0, 2, 22];
+var stockActual2239 = 6379-(data2239[(data2239.length-1)]);
+                                             // Alcohol en Gel 5Lt. - Mat. 3927
+                                                    var data3927 = [0, 0, 0, 0];
+var stockActual3927 = 0-(data3927[(data3927.length-1)]);
+                         // PANEL DIVISORIO-MATERIAL: MDF-75X50 CM - Mat. 5444
+                                                    var data5444 = [0, 0, 0, 0];
+var stockActual5444 = 0-(data5444[(data5444.length-1)]);
+                                        // Mascara Proteccion Facial - Mat. 6130
+                                                    var data6130 = [0, 0, 0, 0];
+var stockActual6130 = 9521-(data6130[(data6130.length-1)]);
+                            // PANTALLA ACRÍLICA DE PROTECCIÓN 90X60 - Mat. 6141
+                                                    var data6141 = [4, 0, 2, 0];
+var stockActual6141 = 353-(data6141[(data6141.length-1)]);
+                                             // Alcohol Etilico 5Lt. - Mat. 6148
+                                                    var data6148 = [0, 0, 0, 0];
+var stockActual6148 = 170-(data6148[(data6148.length-1)]);
+                                             // Alfombra Sanizitante - Mat. 6222
+                                                    var data6222 = [3, 0, 1, 0];
+var stockActual6222 = 312-(data6222[(data6222.length-1)]);
+                                             // REPELENTE DE INSECTOS - Mat. 6302
+                                        var data6302 = [3980, 1756, 3552, 3168];
+var stockActual6302 = 10546-(data6302[(data6302.length-1)]);
+                                        // Termómetro medic. Digital - Mat. 6341
+                                                     var data6341 = [0, 0, 0, 1];
+var stockActual6341 = 209-(data6341[(data6341.length-1)]);
+                                           
